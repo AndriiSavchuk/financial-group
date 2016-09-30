a140e4f86c2bba7f891d2fdf3441e54d5ffcc09b
Andrii Savchuk <andriksr@gmail.com>
2016-09-30 21:41:23 +0300

    Finished section ADVANTAGES

--- app/js/common.js
+++ app/js/common.js
@@ -10,7 +10,15 @@ $(document).ready(function () {
     $(".evaluation .eval-bot-content .info-block").equalHeights();
     $(".accounting .acc-content .info-block").equalHeights();
 
-    // $(".advantages").waypoint(function () {
-    //     $(".advantages").removeClass("card-off").addClass("card-off");
-    // });
+
+    $(".advantages").waypoint(function() {
+
+        $(".advantages .card").each(function (index) {
+            var ths = $(this);
+            setInterval(function () {
+                ths.removeClass("card-off").addClass(".card-on");
+            }, 100*index);
+        })
+    });
 });
+
